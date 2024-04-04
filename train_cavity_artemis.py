43cbde7f5e44682fccdab17737dd200f6ca66726
cfos3120 <cfos3120@uni.sydney.edu>
2024-04-04 21:40:52 +1100

    added memories

--- train_cavity_artemis.py
+++ train_cavity_artemis.py
@@ -328,7 +328,6 @@ if __name__ == '__main__':
     in_queries = dataset.X_for_queries.unsqueeze(0).float().to(device)#.requires_grad_(True)
 
     for epoch in range(training_args['epochs']):
-        print(epoch)
         epoch_start_time = default_timer()
 
         # Set Model to Train
@@ -340,6 +339,8 @@ if __name__ == '__main__':
 
         batch_average_loss = 0
         for batch_n in range(dataset.data_out.shape[0]):
+            
+            print(f'Epoch: {epoch} Batch: {batch_n} Memory Allocated: {torch.cuda.memory_allocated(device) / 1024**3:.2f}GB Memory Cached: {torch.cuda.memory_reservedadded(device) / 1024**3:.2f}GB')
             optimizer.zero_grad()
 
             out_truth   = dataset.data_out[batch_n,...].clone().float().to(device)
