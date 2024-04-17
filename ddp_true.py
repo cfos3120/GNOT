d0b374fa5e522e2ef894437fca21ccb7da2e4b3f
cfos3120 <cfos3120@uni.sydney.edu>
2024-04-17 11:44:06 +1000

    fix

--- ddp_true.py
+++ ddp_true.py
@@ -4,131 +4,75 @@ import math
 from timeit import default_timer
 from argparse import ArgumentParser
 
-from ddp_utils.ddp_utils import *
+from data_storage.loss_recording import total_model_dict, save_checkpoint
 from ddp_utils.data_utils import *
 
-from data_storage.loss_recording import total_model_dict, save_checkpoint
+import os
+import tempfile
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn as nn
+import torch.optim as optim
 
-def train(model,train_loader,optimizer,scheduler,batch_size):
-    device = torch.device(f"cuda:{dist.get_rank()}")
-    train_num_batches = int(math.ceil(len(train_loader.dataset) / float(batch_size)))
-    model.train()
-    # let all processes sync up before starting with a new epoch of training
-    # dist.barrier()
-    criterion = LpLoss_custom()#.to(device)
-    train_loss = 0.0
-    for in_queries, in_keys, out_truth in train_loader:
-        
-        in_queries, in_keys, out_truth = in_queries.to(device), in_keys.to(device), out_truth.to(device)
-        optimizer.zero_grad()
-        output = model(x=in_queries,inputs = in_keys)
-        loss = criterion(output, out_truth)
-        loss.backward()
-        # average gradient as DDP doesn't do it correctly
-        average_gradients(model)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1000)#training_args['grad-clip'])
-        optimizer.step()
-        scheduler.step()
-
-        loss_ = {'loss': torch.tensor(loss.item()).to(device)}
-        train_loss += reduce_dict(loss_)['loss'].item()
-        # cleanup
-        # dist.barrier()
-        # data, target, output = data.cpu(), target.cpu(), output.cpu()
-    train_loss_val = train_loss / train_num_batches
-    return train_loss_val
-    
-def val(model, val_loader,batch_size):
-    device = torch.device(f"cuda:{dist.get_rank()}")
-    val_num_batches = int(math.ceil(len(val_loader.dataset) / float(batch_size)))
-    model.eval()
-    # let all processes sync up before starting with a new epoch of training
-    # dist.barrier()
-    criterion = LpLoss_custom()#.to(device)
-    val_loss = 0.0
-    with torch.no_grad():
-        for in_queries, in_keys, out_truth in val_loader:
-            in_queries, in_keys, out_truth = in_queries.to(device), in_keys.to(device), out_truth.to(device)
-            output = model(x=in_queries,inputs = in_keys)
-            loss = criterion(output, out_truth)
-            loss_ = {'loss': torch.tensor(loss.item()).to(device)}
-            val_loss += reduce_dict(loss_)['loss'].item()
-    val_loss_val = val_loss / val_num_batches
-    return val_loss_val
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
-def function_on_rank(rank, world_size):
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+
+def demo_basic(rank, world_size):
     print(f"Running basic DDP example on rank {rank}.")
     setup(rank, world_size)
 
+    # create model and move it to GPU with id rank
     dataset_args, model_args, training_args = get_default_args()
-
-    dataset_args['file_path'] = '/project/MLFluids/steady_cavity_case_b200_maxU100ms_simple_normalized.npy'
-
     model = get_model(model_args).to(rank)
-    model = DDP(model,device_ids=[rank])#,output_device=rank)
-    dist.barrier()
-    if rank == 0:
-        print('after model allocation:')
-        gpu_print_string,__ = get_gpu_resources()
-        print(gpu_print_string)
+    #model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
 
-    train_loader, val_loader, batch_size = get_dataset(dataset_args)   
-    
-    optimizer = torch.optim.AdamW(model.parameters(), 
-                                  betas=(0.9, 0.999), 
-                                  lr=training_args['base_lr'],
-                                  weight_decay=training_args['weight-decay']
-                                  )
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, 
-                                                    max_lr=training_args['base_lr'], 
-                                                    div_factor=1e4, 
-                                                    pct_start=0.2, 
-                                                    final_div_factor=1e4, 
-                                                    steps_per_epoch=len(train_loader), 
-                                                    epochs=training_args['epochs']
-                                                    )
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
-    # if rank == 0:
-    #     training_run_results = total_model_dict(model_config=model_args, training_config=training_args, data_config=dataset_args)
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    string = f"cuda:{rank}"
+    print(f"Loss on Rank {rank} is {loss.item()}. and device({string}) {torch.cuda.memory_reserved(torch.device(string))}")
 
-    # for epoch in range(training_args['epochs']):
-            
-    #     epoch_start_time = default_timer()
-    #     train_loss_val = train(model,train_loader,optimizer,scheduler,batch_size)
-    #     val_loss_val = val(model,val_loader,batch_size)
-    #     epoch_end_time = default_timer()
-            
-    #     if rank == 0:
-    #         print(f'Rank {rank} epoch {epoch}: {train_loss_val:.2f})/{val_loss_val:.2f}')
-    #         training_run_results.update_loss({'Epoch Time': epoch_end_time - epoch_start_time})
-    #         training_run_results.update_loss({'Training L2 Loss': train_loss_val})
-    #         training_run_results.update_loss({'Evaluation L2 Loss': val_loss_val})
-                
-    #     print(f'Rank {rank} finished training')
-    #     #dist.barrier()
-        
-    #     if rank == 0:
-    #         print('Saving Model...')
-    #         save_checkpoint(training_args["save_dir"], training_args["save_name"], model=model, loss_dict=training_run_results.dictionary, optimizer=optimizer)
-        
-    #     #dist.barrier()
-    dist.barrier()
     cleanup()
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description='GNOT Artemis Training Study')
-    parser.add_argument('--name', type=str, default='test')
-    parser.add_argument('--path', type=str, default= r'C:\Users\Noahc\Documents\USYD\PHD\8 - Github\GNOT\data\steady_cavity_case_b200_maxU100ms_simple_normalized.npy')
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--sub_x', type=int, default=4)
-    parser.add_argument('--inference', type=str, default='True')
-    parser.add_argument('--n_hidden', type=int, default=128)
-    parser.add_argument('--train_ratio', type=float, default=0.7)
-    parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--batch_size', type=int, default=4)
-    args = parser.parse_args()
 
-    run(function_on_rank, 2)
+def run(fn, world_size):
+    mp.spawn(
+        fn,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True
+    )
+
+if __name__ == "__main__":
+    run(demo_basic, 2)
