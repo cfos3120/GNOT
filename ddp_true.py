4f01db2a19fec3e1ff9f2bf0e5ba6492b6286a37
cfos3120 <cfos3120@uni.sydney.edu>
2024-04-17 11:04:41 +1000

    create library

---
+++ ddp_true.py
@@ -0,0 +1,118 @@
+import torch
+import numpy as np
+import math
+from timeit import default_timer
+from argparse import ArgumentParser
+
+from ddp_utils.ddp_utils import *
+from ddp_utils.data_utils import *
+
+from data_storage.loss_recording import total_model_dict, save_checkpoint
+
+def train(model,train_loader,optimizer,scheduler,batch_size):
+    device = torch.device(f"cuda:{dist.get_rank()}")
+    train_num_batches = int(math.ceil(len(train_loader.dataset) / float(batch_size)))
+    model.train()
+    # let all processes sync up before starting with a new epoch of training
+    # dist.barrier()
+    criterion = LpLoss_custom()#.to(device)
+    train_loss = 0.0
+    for in_queries, in_keys, out_truth in train_loader:
+        
+        in_queries, in_keys, out_truth = in_queries.to(device), in_keys.to(device), out_truth.to(device)
+        optimizer.zero_grad()
+        output = model(x=in_queries,inputs = in_keys)
+        loss = criterion(output, out_truth)
+        loss.backward()
+        # average gradient as DDP doesn't do it correctly
+        average_gradients(model)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1000)#training_args['grad-clip'])
+        optimizer.step()
+        scheduler.step()
+
+        loss_ = {'loss': torch.tensor(loss.item()).to(device)}
+        train_loss += reduce_dict(loss_)['loss'].item()
+        # cleanup
+        # dist.barrier()
+        # data, target, output = data.cpu(), target.cpu(), output.cpu()
+    train_loss_val = train_loss / train_num_batches
+    return train_loss_val
+    
+def val(model, val_loader,batch_size):
+    device = torch.device(f"cuda:{dist.get_rank()}")
+    val_num_batches = int(math.ceil(len(val_loader.dataset) / float(batch_size)))
+    model.eval()
+    # let all processes sync up before starting with a new epoch of training
+    # dist.barrier()
+    criterion = LpLoss_custom()#.to(device)
+    val_loss = 0.0
+    with torch.no_grad():
+        for in_queries, in_keys, out_truth in val_loader:
+            in_queries, in_keys, out_truth = in_queries.to(device), in_keys.to(device), out_truth.to(device)
+            output = model(x=in_queries,inputs = in_keys)
+            loss = criterion(output, out_truth)
+            loss_ = {'loss': torch.tensor(loss.item()).to(device)}
+            val_loss += reduce_dict(loss_)['loss'].item()
+    val_loss_val = val_loss / val_num_batches
+    return val_loss_val
+
+
+
+def function_on_rank(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
+
+    dataset_args, model_args, training_args = get_default_args()
+
+    model = get_model(model_args).to(rank)
+    model = DDP(model,device_ids=[rank])#,output_device=rank)
+
+    if rank == 0:
+        print('after model allocation:')
+        gpu_print_string,__ = get_gpu_resources()
+        print(gpu_print_string)
+
+    train_loader, val_loader, batch_size = get_dataset(dataset_args)   
+    
+    optimizer = torch.optim.AdamW(model.parameters(), 
+                                  betas=(0.9, 0.999), 
+                                  lr=training_args['base_lr'],
+                                  weight_decay=training_args['weight-decay']
+                                  )
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, 
+                                                    max_lr=training_args['base_lr'], 
+                                                    div_factor=1e4, 
+                                                    pct_start=0.2, 
+                                                    final_div_factor=1e4, 
+                                                    steps_per_epoch=len(train_loader), 
+                                                    epochs=training_args['epochs']
+                                                    )
+
+    if rank == 0:
+        training_run_results = total_model_dict(model_config=model_args, training_config=training_args, data_config=dataset_args)
+
+    for epoch in range(training_args['epochs']):
+            
+        epoch_start_time = default_timer()
+        train_loss_val = train(model,train_loader,optimizer,scheduler,batch_size)
+        val_loss_val = val(model,val_loader,batch_size)
+        epoch_end_time = default_timer()
+            
+        if rank == 0:
+            print(f'Rank {rank} epoch {epoch}: {train_loss_val:.2f})/{val_loss_val:.2f}')
+            training_run_results.update_loss({'Epoch Time': epoch_end_time - epoch_start_time})
+            training_run_results.update_loss({'Training L2 Loss': train_loss_val})
+            training_run_results.update_loss({'Evaluation L2 Loss': val_loss_val})
+                
+        print(f'Rank {rank} finished training')
+        dist.barrier()
+        
+        if rank == 0:
+            print('Saving Model...')
+            save_checkpoint(training_args["save_dir"], training_args["save_name"], model=model, loss_dict=training_run_results.dictionary, optimizer=optimizer)
+        
+        dist.barrier()
+        cleanup(rank)
+
+if __name__ == "__main__":
+    run(function_on_rank, 2)
---
+++ ddp_utils/ddp_utils.py
@@ -0,0 +1,106 @@
+import os
+import tempfile
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn as nn
+import torch.optim as optim
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+'''
+To run DDP:
+define a function to run in parallel ('fn')
+
+in __main__ run 'run' which features MP Spawn
+
+1. in the function 'fn' include 'setup()'
+2. wrap the model in DDP
+3. at the end of the function: 'cleanup()'
+'''
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def cleanup():
+    dist.destroy_process_group()
+
+
+class ToyModel(nn.Module):
+    def __init__(self):
+        super(ToyModel, self).__init__()
+        self.net1 = nn.Linear(10, 10)
+        self.relu = nn.ReLU()
+        self.net2 = nn.Linear(10, 5)
+
+    def forward(self, x):
+        return self.net2(self.relu(self.net1(x)))
+
+
+def demo_basic(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
+
+    # create model and move it to GPU with id rank
+    model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss = loss_fn(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    string = f"cuda:{rank}"
+    print(f"Loss on Rank {rank} is {loss.item()}. and device({string}) {torch.cuda.memory_reserved(torch.device(string))}")
+
+    cleanup()
+
+
+def run(fn, world_size):
+    mp.spawn(
+        fn,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True
+    )
+
+def get_gpu_resources():
+    if torch.cuda.is_available():
+        print_string1 = '   Memory Reserved:'
+        print_string2 = '   Memory Allocated:'
+        for gpu_n in range(torch.cuda.device_count()):
+            gpu_n_str = f"cuda:{gpu_n}"
+            print_string1 += f'GPU{gpu_n} {torch.cuda.memory_reserved(torch.device(gpu_n_str)) / 1024**3:5.2f}GB '
+            print_string2 += f'GPU{gpu_n} {torch.cuda.memory_allocated(torch.device(gpu_n_str)) / 1024**3:5.2f}GB '
+    else:
+        print_string1, print_string2 = '        No GPUs', '        No GPUs'
+
+    return print_string1, print_string2
+
+def average_gradients(model):
+    size = float(dist.get_world_size())
+    for param in model.parameters():
+        dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
+        param.grad.data /= size
+
+def reduce_dict(input_dict, average=True):
+    world_size = float(dist.get_world_size())
+    names, values = [], []
+    for k in sorted(input_dict.keys()):
+        names.append(k)
+        values.append(input_dict[k])
+    values = torch.stack(values, dim=0)
+    dist.all_reduce(values, op=dist.ReduceOp.SUM)
+    if average:
+        values /= world_size
+    reduced_dict = {k: v for k, v in zip(names, values)}
+    return reduced_dict
